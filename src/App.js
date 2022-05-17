7eb399a5dd72bec520de453ec278342e655d31d9
sohel Ahamed <sohel770hossain@gmail.com>
2022-05-17 12:39:14 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -14,36 +14,35 @@ import Register from './Compontes/Login/Register';
 import Cheackout from './Compontes/Checkout/Cheackout';
 import RequireAuth from './Compontes/Login/RequireAuth/RequireAuth';
 import About from './Compontes/About/About';
-import Logins from './Compontes/Login/Logins';
+
 
 
 
 
 function App() {
   return (
-  <div> 
-    <Header></Header>
-  
-  <Routes> 
-    <Route path='/' element={<Home></Home>}></Route>
-    <Route path='/home' element={<Home></Home>}></Route>
-    <Route path='/services' element={<Services></Services>}></Route>
-    <Route path='/services/:serviceId' element={<ServicesDetails></ServicesDetails>}></Route>
-    <Route path='/checkout' element={
-     <RequireAuth><Cheackout></Cheackout></RequireAuth>
+    <div>
+      <Header></Header>
 
-    }></Route>
-    <Route path='experts' element={<Experts></Experts>}></Route>
-   <Route path='/blog' element={<Bloge></Bloge>}></Route>
-   {/* <Route path='/login' element={<Login></Login>}></Route> */}
-   <Route path='/login' element={<Logins></Logins>}></Route>
-   <Route path='register' element={<Register></Register>}></Route>
-   <Route path='/about' element={<About></About>}></Route>
-   <Route path="*" element={<NotFund></NotFund>}></Route>
-  </Routes>
-  <Footer></Footer>
-  </div>
-      
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/services' element={<Services></Services>}></Route>
+        <Route path='/services/:serviceId' element={<ServicesDetails></ServicesDetails>}></Route>
+        <Route path='/checkout' element={
+          <RequireAuth><Cheackout></Cheackout></RequireAuth>
+
+        }></Route>
+        <Route path='experts' element={<Experts></Experts>}></Route>
+        <Route path='/blog' element={<Bloge></Bloge>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='register' element={<Register></Register>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path="*" element={<NotFund></NotFund>}></Route>
+      </Routes>
+      <Footer></Footer>
+    </div>
+
   );
 }
 
--- src/Compontes/Login/Login.js
+++ src/Compontes/Login/Login.js
@@ -1,95 +1,60 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import './Login.css'
-import google from '../../images/google.png'
-import { useLocation, useParams } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
-import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
 import SocialLogin from './SocialLogin/SocialLogin';
-import Loading from '../Loading/Loading';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
 
 
 const Login = () => {
+const emailRef=useRef('')
+const passwordRef=useRef('')
 const navigate=useNavigate()
- const emailRef=useParams('')
- const passwordRef=useParams('')
- const [sendPasswordResetEmail] = useSendPasswordResetEmail(
-  auth
-);
- const [
-  signInWithEmailAndPassword,
-  user,
-  loading,
-  error,
-] = useSignInWithEmailAndPassword(auth);
-
 const location=useLocation()
+
 let from = location.state?.from?.pathname || "/";
 
-let errorElement;
-if (error) {
-    errorElement = <p className="text-danger">Error: {error.message}</p>
+const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword(auth);
 
-}
-if(loading){
-  return <Loading></Loading>
-}
-const handleonSubmit=event=>{
-    event.preventDefault();
-    const email=emailRef.current.value
-    const password=passwordRef.current.value
-    console.log(email, password);
+
+  if(user){
+    navigate(from, { replace: true })
+  }
+const handleonSubmit=event => {
+    event.preventDefault()
+    const email=emailRef.current.value;
+    const password = passwordRef.current.value;
+
     signInWithEmailAndPassword(email, password)
 }
-if(user){
-  navigate(from, { replace: true });
-}
 
-const resetPassword =async () => {
-  const email=emailRef.current.value
-  if(email){
-    await sendPasswordResetEmail(email);
-  toast('Sent email');
-  }else{
-    toast('Please Enter Your Email')
-  }
+const navigateRegister=event => {
+    navigate('/register')
 }
-
-const navigateRegister=event=>{
-  navigate('/register')
-}
-
     return (
-        <div className="container w-50 mx-auto login">
-            <h3 className='text-primary text-center'>Please Login</h3>
-           <Form onSubmit={handleonSubmit}>
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
-  </Form.Group>
+        <div className="container w-50 mx-auto">
+            <h3 className="text-primary text-center">Please Login</h3>
+            <Form onSubmit={handleonSubmit}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
+                </Form.Group>
 
-  <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
-  </Form.Group>
-
-
-<p className="">New Strength Center
- <span className='text-primary' onClick={navigateRegister}> Please Register</span></p>
-
-<p className="">Forget Password <span className='text-primary' onClick={resetPassword}>Reset Password</span></p>
-  
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-  {errorElement}
-</Form>
-<SocialLogin></SocialLogin>
-<ToastContainer />
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+            <p >New Strength Center <span className='text-primary' onClick={navigateRegister} >Please Register</span> </p>
+            <SocialLogin></SocialLogin>
         </div>
     );
 };
